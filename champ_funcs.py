5d7c9ea381e7f238877c5bb6a208b4691b580681
ryanahardy <ryan.hardy@colorado.edu>
2016-11-12 18:05:54 -0700

    Wrote a driver shell script for directing outputs. Code will now create output directories and generate over 800 figures when run.

--- champ_funcs.py
+++ champ_funcs.py
@@ -4,6 +4,20 @@
 # Created: 20161110
 # Modified: 20161110
 
+import numpy as np
+import matplotlib.pyplot as plt
+from textblob import TextBlob
+from pandas import DataFrame
+import pandas as pd
+import matplotlib
+import json
+from textwrap import wrap
+from scipy import stats
+import re
+import sys
+from champ_funcs import *
+
+
 def standard_major(given_major):
     major = given_major.lower()
     if given_major == "macs":
@@ -21,40 +35,3 @@ def standard_country(given_country):
 def standard_languages(given_language):
     language = given_country.lower()
     return country
-
-def gauge_chart_ordinal_cross(responses, categories):
-	stack = DataFrame(columns=np.arange(1, 7), index=categories.columns)
-	for i in range(categories.shape[-1]):
-		stack.ix[i] = np.histogram(responses[categories.ix[:, i]], 
-			np.arange(1, 8), normed=True)[0]
-	(100*stack[::-1].ix[:,  ::-1]).plot(kind='barh', stacked=True, 
-		width=1, edgecolor='w', colors=plt.cm.RdBu_r(np.linspace(0.25, 0.75, 6)), 
-		align='edge', figsize=(12, 6), legend=False)
-	plt.title("\n".join(wrap(dic['Question_Text']\
-			[responses.name==dic['Fall_2016_Question_Code']][0], 88)), size='medium')
-	for i in range(6):
-		plt.axvline(np.cumsum(np.histogram(responses[categories.ix[:, 0]], 
-		np.arange(1, 8), normed=True)[0][::-1])[i]*100, color='lightgray')
-	plt.axis('tight')
-	plt.xlabel('%')
-	plt.legend(bbox_to_anchor=(0., 1.0, 1.12, -0.25))
-	plt.subplots_adjust(left=0.15, right=0.92)
-	
-
-def gauge_chart_categorical_cross(responses, categories, values):
-	stack = DataFrame(columns=np.arange(1, 7), index=categories.columns)
-	for i in range(categories.shape[-1]):
-		stack.ix[i] = np.histogram(responses[categories.ix[:, i]], 
-			np.arange(1, 8), normed=True)[0]
-	(100*stack[::-1].ix[:,  ::-1]).plot(kind='barh', stacked=True, 
-		width=1, edgecolor='w', colors=plt.cm.RdBu_r(np.linspace(0.25, 0.75, 6)), 
-		align='edge', figsize=(12, 6), legend=False)
-	plt.title("\n".join(wrap(dic['Question_Text']\
-			[responses.name==dic['Fall_2016_Question_Code']][0], 88)), size='medium')
-	for i in range(6):
-		plt.axvline(np.cumsum(np.histogram(responses[categories.ix[:, 0]], 
-		np.arange(1, 8), normed=True)[0][::-1])[i]*100, color='lightgray')
-	plt.axis('tight')
-	plt.xlabel('%')
-	plt.legend(bbox_to_anchor=(0., 1.0, 1.12, -0.25))
-	plt.subplots_adjust(left=0.15, right=0.92)
