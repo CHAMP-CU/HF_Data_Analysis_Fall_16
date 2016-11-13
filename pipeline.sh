aa7fb1044e2140a8b129fc5ea11db80abd3ec09d
ryanahardy <ryan.hardy@colorado.edu>
2016-11-13 13:30:39 -0700

    Piepline driver shell script

---
+++ pipeline.sh
@@ -0,0 +1,16 @@
+analysis_dir='../results'
+mkdir $analysis_dir
+mkdir $analysis_dir/figs
+mkdir $analysis_dir/tables
+mkdir ../results/figs/gender
+mkdir ../results/figs/experience
+mkdir ../results/figs/national
+mkdir ../results/figs/champ
+mkdir ../results/figs/repeat
+mkdir ../results/figs/cm
+mkdir ../results/figs/height
+mkdir ../results/figs/all
+
+python champ.py > $analysis_dir/tables/results.txt
+grep -E '^\d|^[A-Z]|All' $analysis_dir/tables/results.txt > $analysis_dir/tables/toplines.txt
+grep -E '^\d|^[A-Z]|\*|All' $analysis_dir/tables/results.txt > $analysis_dir/tables/significant.txt
