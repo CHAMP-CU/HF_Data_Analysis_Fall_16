9b369df0f56d27a5e7a27ab2c8b82d2da7fbbae1
ryanahardy <ryan.hardy@colorado.edu>
2016-11-13 21:58:19 -0700

    Added statistical tests and tables for continuous data

--- champ.py
+++ champ.py
@@ -206,11 +206,12 @@ plt.title('\n'.join(wrap("Could the volume of the following spaces be smaller "+
 #rf = DataFrame(responses)
 
 #Create tables
-mask = (dic['Data_type']=="Ordinal")+(dic['Data_type']=="Categorical")
+'''mask = (dic['Data_type']=="Ordinal")+(dic['Data_type']=="Categorical")
 mask+= (dic['Data_type']=="Binary")+(dic['Data_type']=="Count")
 mask+= (dic['Data_type']=="Free Response")+(dic['Data_type']=="Comment")
 mask+= (dic['Data_type']=="Free response")
 mask+= (dic['Data_type']=="Multiple selection")+(dic['Data_type']=="Multiple Selection")
+mask+= (dic['Data_type']=="Continuous")'''
 
 #Exclude questions with fewer than 8 responses
 mask *= (df.count(axis=0) > 8)#+(df.sum(0).str.count('NaN') < len(df)-8)
@@ -255,7 +256,7 @@ for i in np.argsort(np.array(dic['Order_Asked'][mask], int))[1:]:
 			#total = (subframe.ix[:, i].ix[categories.ix[:, j]]!='NaN').sum()
 			#total_not = (subframe.ix[:, i].ix[-categories.ix[:, j]]!='NaN').sum()
 			total = subframe.ix[:, i].ix[categories.ix[:, j]].valid().count()
-			total_not = subframe.ix[:, i].ix[categories.ix[:, j]].valid().count()
+			total_not = subframe.ix[:, i].ix[-categories.ix[:, j]].valid().count()
 			if False:
 				for k in range(len(responsetypes)):
 					width[k] = (responsetypes[k]==subframe.ix[:, i].ix[categories.ix[:, j]].astype(str)).sum()*1./total
@@ -269,7 +270,6 @@ for i in np.argsort(np.array(dic['Order_Asked'][mask], int))[1:]:
 			
 			print '%21s\t%i' % (category_names[j], total) + '\t%3.1f%%'*len(responsetypes) % tuple(width*100)+'\t%3.2f' % (pval)+'*'*(pval < 0.05)
 		print
-		
 	elif (dic['Data_type'][mask][i] == 'Count')*(i > 5):
 		
 		responsetypes = np.array(np.unique(subframe.ix[:, i].dropna()), int)
@@ -286,19 +286,29 @@ for i in np.argsort(np.array(dic['Order_Asked'][mask], int))[1:]:
 		for j in range(len(subframe.ix[:, i])):
 			if (subframe.ix[:, i][j] !='NaN') and (subframe.ix[:, i][j]== subframe.ix[:, i][j]):
 				print 
-				cat_tags = '%s, '*(categories.ix[j].sum()-1) % tuple(category_names[categories.ix[j]][1:])
-				print '\n'.join(wrap(cat_tags, 70))
+				cat_tags = '\t\t'+'%s, '*(categories.ix[j].sum()-1) % tuple(category_names[categories.ix[j]][1:])
+				
 				print '\t'+'\n\t'.join(wrap(subframe.ix[:, i][j], 70))
+				print '\n\t\t'.join(wrap(cat_tags, 70))
 	elif (dic['Data_type'][mask][i].lower() == 'multiple selection'):
 		selections = -DataFrame(index=df.index, columns= dic['Data_values'][mask][i].split(';'), 
 				dtype=bool)
-		for j in range(len(df)):
+		for j in np.where(subframe.ix[:, i]==subframe.ix[:, i])[0]:
 			for k in range(selections.shape[-1]):
-				selections.ix[j, k] =  selections.columns[k] in [subframe.ix[:, i][j]]
+				selections.ix[j, k] =  selections.columns[k] in subframe.ix[:, i][j]
+		
 		for j in range(len(selections.columns)):
-			print '%61s\t' % (selections.columns[j]) + '\t%2.1f%%' % (100*selections.mean(0)[j])
-
-
+			print '%70s\t' % (selections.columns[j]) + '\t%2.1f%%' % (100*selections.mean(0)[j])
+	elif dic['Data_type'][mask][i] == 'Continuous':
+		print dic['Data_values'][mask][i]
+		print "Category\t\tn\tMean\tStd.\tMin.\t25%\t50%\t75%\tMax.\tp-value"
+		for j in range(len(category_names)):
+			description = subframe.ix[:, i].ix[categories.ix[:, j]].describe()
+			pop1 = subframe.ix[:, i].ix[categories.ix[:, j]].dropna()
+			pop2 = subframe.ix[:, i].ix[-categories.ix[:, j]].dropna()
+			pval = stats.ttest_ind(pop1, pop2)[1]
+			print '%21s\t%i' % (category_names[j], description['count'])+'\t%3.1f'*(len(description)-1) % tuple(description[1:])+ '\t%3.2f' % (pval)+'*'*(pval < 0.05)
+		print
 
 def gauge_chart_ordinal_cross(responses, categories):
 	values = dic['Data_values'][responses.name==dic['Fall_2016_Question_Code']][0]
@@ -339,6 +349,25 @@ def gauge_chart_categorical_cross(responses, categories):
 	plt.xticks(np.arange(0, 101, 10), ('%i%% '*11 % tuple(np.arange(0, 101, 10))).split())
 	plt.legend(bbox_to_anchor=(0.55, -0.05, 0.5, 0), ncol=len(values), fontsize='small')
 	plt.subplots_adjust(left=0.18, right=0.92)
+	
+
+def gauge_chart_categorical_cross(responses, categories):
+	values = dic['Data_values'][responses.name==dic['Fall_2016_Question_Code']][0].split(';')
+	stack = DataFrame(columns=values, index=categories.columns)
+	for i in range(len(stack)):
+		for j in range(len(stack.T)):
+			stack.ix[i, j] = (np.array(responses[categories.ix[:, i]], str)==values[j]).sum()
+	(100*stack.T/stack.sum(1)).T[::-1].plot(kind='barh', stacked=True, width=1, 
+		edgecolor='w', legend=False, align='edge', figsize=(12, 6))
+	plt.title("\n".join(wrap(dic['Question_Text']\
+			[responses.name==dic['Fall_2016_Question_Code']][0], 88)), size='medium')
+	for i in range(len(stack.T)):
+		plt.axvline(np.cumsum((stack.T/stack.sum(1)).T.ix[0])[::-1][i]*100, color='lightgray')
+	plt.axis('tight')
+	plt.xticks(np.arange(0, 101, 10), ('%i%% '*11 % tuple(np.arange(0, 101, 10))).split())
+	plt.legend(bbox_to_anchor=(0.55, -0.05, 0.5, 0), ncol=len(values), fontsize='small')
+	plt.subplots_adjust(left=0.18, right=0.92)
+
 
 makefigs = True
 if makefigs:
@@ -365,7 +394,17 @@ if makefigs:
 				gauge_chart_categorical_cross(subframe.ix[:, i], subcats[j])
 				plt.savefig(output_path+"/figs/"+subcat_names[j]+'/'+'%03i' % i)
 				plt.close()
-
+		elif (dic['Data_type'][mask][i] == 'Categorical')+(dic['Data_type'][mask][i] == 'Binary'):
+			gauge_chart_categorical_cross(subframe.ix[:, i], categories)
+			plt.savefig(output_path+"/figs/all/"+'%03i' % i)
+			plt.close()
+			gauge_chart_categorical_cross(subframe.ix[:, i], DataFrame(categories.All))
+			plt.savefig(output_path+"/figs/topline/"+'%03i' % i)
+			plt.close()
+			for j in range(len(subcats)):
+				gauge_chart_categorical_cross(subframe.ix[:, i], subcats[j])
+				plt.savefig(output_path+"/figs/"+subcat_names[j]+'/'+'%03i' % i)
+				plt.close()
 
 
 sys.exit()
