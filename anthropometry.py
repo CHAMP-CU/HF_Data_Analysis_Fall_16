7f38f55a1ff99e58d31c9486123937e59d0e1b44
ryanahardy <ryan.hardy@colorado.edu>
2016-12-01 23:01:30 -0700

    Label y-coordinates in anthropometry.py

--- anthropometry.py
+++ anthropometry.py
@@ -37,19 +37,19 @@ for i in range(3):
 		plt.axvline([76.6,22.1,35.8][i], linestyle='-', color=orioncol, lw=2)
 
 		plt.annotate('5th pct. \nJapanese female',
-					(stats.norm.ppf(0.05, jf[0, i], jf[1, i]), 0.125),
+					(stats.norm.ppf(0.05, jf[0, i], jf[1, i]), 0.8/np.sqrt(2*np.pi*jf[1, i]**2)),
 					va='top', ha='left', rotation=0, color='w',
-					size='small', weight='bold', bbox=dict(boxstyle="round", lw=0, fc=isscol, alpha=0.5))
+					size='small', weight='bold', bbox=dict(boxstyle="round", lw=0, fc=isscol, alpha=0.75))
 		plt.annotate('95th pct. \nAmerican male',
-					(stats.norm.ppf(0.95, am[0, i], am[1, i]), 0.08),
+					(stats.norm.ppf(0.95, am[0, i], am[1, i]),  1/np.sqrt(2*np.pi*am[1, i]**2)),
 					va='top', ha='right', rotation=0, color='w',
-					size='small', weight='bold', bbox=dict(boxstyle="round", lw=0, fc=isscol, alpha=0.5))
-		plt.annotate('Orion Upper Limit', ([76.6,22.1,35.8][i], 0.14),
+					size='small', weight='bold', bbox=dict(boxstyle="round", lw=0, fc=isscol, alpha=0.75))
+		plt.annotate('Orion Upper Limit', ([76.6,22.1,35.8][i], 0.7/np.sqrt(2*np.pi*am[1, i]**2)),
 					va='bottom', ha='right', rotation=0, color='w',
-					size='small', weight='bold', bbox=dict(boxstyle="round", lw=0, fc=orioncol, alpha=0.5))
-		plt.annotate('Orion Lower Limit', ([58.5,14.9,25.6][i], 0.17),
+					size='small', weight='bold', bbox=dict(boxstyle="round", lw=0, fc=orioncol, alpha=0.75))
+		plt.annotate('Orion Lower Limit', ([58.5,14.9,25.6][i], 0.5/np.sqrt(2*np.pi*jf[1, i]**2)),
 					va='bottom', ha='left', rotation=0, color='w',
-					size='small', weight='bold', bbox=dict(boxstyle="round", lw=0, fc=orioncol, alpha=0.5))
+					size='small', weight='bold', bbox=dict(boxstyle="round", lw=0, fc=orioncol, alpha=0.75))
     if i ==1:
         	plt.xlim(13, 23)
     plt.subplots_adjust(left=0.15, bottom=0.1, hspace=0.33)
